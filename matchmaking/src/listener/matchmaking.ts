10af155f3313f7980cf7f461cd6945f76ca4322e
Simon <simon.sorgenfrei@chainofalliance.com>
2025-01-16 10:03:13 +0100

    Chore adapt mm to onchain node selector

--- matchmaking/src/listener/matchmaking.ts
+++ matchmaking/src/listener/matchmaking.ts
@@ -3,12 +3,11 @@ import { Ticket } from '../ticket.js';
 import { guid, now } from '../javascript-helper.js';
 import { Match } from '../match.js';
 import { PostgresService } from '../postgres.js';
-import { getActiveNodes, queryDappInfo } from '../services/blockchain/queries.js';
 import { addSession } from '../services/blockchain/operations.js';
-import { ActiveNode, DappInfo, MatchData, Participant, ParticipantRole } from '../services/blockchain/types.js';
-import { NODES_NEEDED } from '../env.js';
+import { MatchData } from '../services/blockchain/types.js';
 import { logger } from '../logger.js';
 import { formatter } from 'postchain-client';
+import { queryConnectionDetails } from '../services/blockchain/queries.js';
 
 let postgres: PostgresService;
 
@@ -55,78 +54,19 @@ async function resolve(ticket1: Ticket, ticket2: Ticket | null) {
         ticket1.waitForServer();
         ticket2?.waitForServer();
         const sessionId = guid();
-        const nodes = await getActiveNodes();
-        log('info', `Checking ${nodes.length} nodes`);
 
-        const dappInfo = await queryDappInfo();
-        if (dappInfo == undefined)
-            throw new Error("Cannot query dapp info");
-
-        let chosenNodes: ActiveNode[] = [];
-        let chosenCount: number = 0;
-        const nodesNeeded = NODES_NEEDED();
-
-        nodes.sort(() => Math.random() - 0.5);
-        for (let node of nodes) {
-            if (await isNodeHealthy(node, dappInfo)) {
-                chosenNodes.push(node);
-                chosenCount++;
-                log('info', `Node ${node.address.toString('hex')} is healthy`);
-
-                if (chosenCount == nodesNeeded)
-                    break;
-            } else {
-                log('info', `Node ${node.address.toString('hex')} is not healthy`);
-            }
-        }
-
-        if (chosenNodes.length < nodesNeeded) {
-            log('error', `Not enough nodes available, needed ${nodesNeeded}..`);
-            ticket1.close();
-            ticket2?.close();
-            return;
-        }
-
-        const participants: Participant[] = [];
+        const participants: Buffer[] = [];
         const matchData: MatchData[] = [];
-        const mainNode: ActiveNode = chosenNodes.pop()!;
 
-        participants.push(
-            {
-                address: "",
-                pubkey: formatter.toBuffer(ticket1.address),
-                role: ParticipantRole.PLAYER
-            },
-            {
-                address: mainNode.url,
-                pubkey: mainNode.address,
-                role: ParticipantRole.MAIN
-            }
-        );
-
-        chosenNodes.forEach(e => participants.push(
-            {
-                address: e.url,
-                pubkey: e.address,
-                role: ParticipantRole.OBSERVER
-            }
-        ));
+        participants.push(formatter.toBuffer(ticket1.address));
 
         matchData.push({
             Id: ticket1.address
         });
 
         if (ticket2) {
-            participants.push(
-                {
-                    address: "",
-                    pubkey: formatter.toBuffer(ticket2.address),
-                    role: ParticipantRole.PLAYER
-                }
-            );
-            matchData.push({
-                Id: ticket2.address
-            });
+            participants.push(formatter.toBuffer(ticket2.address));
+            matchData.push({Id: ticket2.address});
         }
 
         await addSession(sessionId, participants, matchData);
@@ -134,10 +74,16 @@ async function resolve(ticket1: Ticket, ticket2: Ticket | null) {
         ticket1.match(sessionId);
         ticket2?.match(sessionId);
 
+        const url = await queryConnectionDetails(sessionId);
+
+        if(url == undefined) {
+            throw new Error("Cannot query connection details");
+        }
+
         let match: Match = {
             id: sessionId,
             isAI: ticket2 == null,
-            serverDetails: mainNode.url,
+            serverDetails: url!,
             createdAt: now()
         };
 
@@ -158,34 +104,4 @@ export function log(level: any, message: string): string {
     const id = guid();
     logger.log(level, `MatchmakingListener ${id} > ${message}`);
     return id;
-}
-
-async function isNodeHealthy(node: ActiveNode, info: DappInfo) {
-    try {
-        const headers: Headers = new Headers();
-        headers.set('Content-Type', 'application/json');
-        headers.set('Accept', 'application/json');
-
-        log('debug', `Fetching ${node.url}/status`);
-        const request: RequestInfo = new Request(`${node.url}/status`, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify({
-                uid: info.uid,
-                version: info.version
-            })
-        })
-
-        const response = await fetch(request);
-
-        if (response.status == 200) {
-            const json = await response.json();
-            return !json.error;
-        }
-
-        return false;
-    } catch (error) {
-        log('debug', `Failed to fetch ${node.url} ${error}`);
-        return false;
-    }
 }
