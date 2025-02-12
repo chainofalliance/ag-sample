081dbd7b7921cc5f953779d33fb0ada9a79d5f69
Simon <simon.sorgenfrei@chainofalliance.com>
2025-02-12 16:26:08 +0100

    use ec managed validator

--- contract/ignition/modules/TicTacToe.ts
+++ contract/ignition/modules/TicTacToe.ts
@@ -5,7 +5,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const TicTacToeModule = buildModule("TicTacToeModule", (m) => {
   const ticTacToe = m.contract("TicTacToe", [
-    "0x0d5Af8A0F885F6E9e573cCa9b67fbE69a98B06eE", // validator
+    "0xFdB06dB69e45a732bDF0B44FA590dAcb33deE281", // validator
     "9E32CFBBA7C62BAA311D110C3212749136EF4BFC0595078738FEE2BF357692BD", // rid
     "0xc328ff8De8A468D090A48dE40ae8e623B0725Ed8" // default admin
   ]
