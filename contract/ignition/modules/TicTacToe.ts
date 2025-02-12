81a4c590ed6c1c1f1e304153c3663047552d7820
Simon <simon.sorgenfrei@chainofalliance.com>
2025-02-12 16:17:18 +0100

    Chore update validator

--- contract/ignition/modules/TicTacToe.ts
+++ contract/ignition/modules/TicTacToe.ts
@@ -5,7 +5,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const TicTacToeModule = buildModule("TicTacToeModule", (m) => {
   const ticTacToe = m.contract("TicTacToe", [
-    "0x0b9eD89C206468bcd63e478f35614e06a97142D8", // validator
+    "0x0d5Af8A0F885F6E9e573cCa9b67fbE69a98B06eE", // validator
     "9E32CFBBA7C62BAA311D110C3212749136EF4BFC0595078738FEE2BF357692BD", // rid
     "0xc328ff8De8A468D090A48dE40ae8e623B0725Ed8" // default admin
   ]
