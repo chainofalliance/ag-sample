47b88a211ed60ca3d4cc57198460e6572653daa9
Simon <simon.sorgenfrei@chainofalliance.com>
2025-03-03 10:38:02 +0100

    Init new client

---
+++ new_client/TicTacToe/Assets/AppKitInit.cs
@@ -0,0 +1,41 @@
+using Reown.AppKit.Unity;
+using Reown.Core.Common.Logging;
+using Reown.Sign.Unity;
+using UnityEngine;
+
+public class AppKitInit : MonoBehaviour
+{
+    private async void Start()
+    {
+        ReownLogger.Instance = new UnityLogger();
+
+        // AppKit configuration
+        var appKitConfig = new AppKitConfig
+        {
+            // Project ID from https://cloud.reown.com/
+            projectId = "7457ffbc1346fad3a828e49743fba2e0",
+            metadata = new Metadata(
+                "AppKit Unity",
+                "AppKit Unity Sample",
+                "https://reown.com",
+                "https://raw.githubusercontent.com/reown-com/reown-dotnet/main/media/appkit-icon.png",
+                new RedirectData
+                {
+                    // Used by native wallets to redirect back to the app after approving requests
+                    Native = "appkit-sample-unity://"
+                }
+            )
+        };
+
+        Debug.Log("[AppKit Init] Initializing AppKit...");
+
+        await AppKit.InitializeAsync(
+            appKitConfig
+        );
+    }
+
+    public void OpenModal()
+    {
+        AppKit.OpenModal();
+    }
+}
