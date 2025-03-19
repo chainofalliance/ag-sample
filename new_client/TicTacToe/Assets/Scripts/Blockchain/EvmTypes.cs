6daf0e475eddc66ae4742c107a76e29fe5a3114a
Simon <simon.sorgenfrei@chainofalliance.com>
2025-03-19 12:05:27 +0100

    Chore add struct type for batch claim

--- new_client/TicTacToe/Assets/Scripts/Blockchain/EvmTypes.cs
+++ new_client/TicTacToe/Assets/Scripts/Blockchain/EvmTypes.cs
@@ -26,6 +26,32 @@ public class EvmTypes
         }
     }
 
+    [Struct("ClaimData")]
+    public class ClaimData
+    {
+        [Parameter("bytes", "eventData", 1)]
+        public byte[] EventData { get; set; }
+
+        [Parameter("tuple", "eventProof", 2)]
+        public EventProof EventProof { get; set; }
+
+        [Parameter("bytes", "blockHeader", 3)]
+        public byte[] BlockHeader { get; set; }
+
+        [Parameter("bytes[]", "signatures", 4)]
+        public List<byte[]> Signatures { get; set; }
+
+        [Parameter("address[]", "signers", 5)]
+        public List<string> Signers { get; set; }
+
+        [Parameter("tuple", "extraProof", 6)]
+        public ExtraMerkleProof ExtraProof { get; set; }
+
+        [Parameter("bytes", "encodedData", 7)]
+        public byte[] EncodedData { get; set; }
+    }
+
+
     [Struct("Proof")]
     public class EventProof
     {
